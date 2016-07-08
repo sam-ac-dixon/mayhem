91fb2f717c9e1661b06b4773cd1eefe99d432327
Alex Close <adclose93@gmail.com>
2016-07-08 20:56:58 +0100

    Created basic world background using tiles. Used the hexagon tile from slither.io as a placeholder.

---
+++ client/Assets/World/Map.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Mayhem.World
+{
+    public class Map : MonoBehaviour
+    {
+        public Vector2 Size;
+        public GameObject MapTilePrefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            float tileWidth = MapTilePrefab.GetComponent<RectTransform>().rect.width;
+            float tileHeight = MapTilePrefab.GetComponent<RectTransform>().rect.height;
+
+            for (float x = 0; x < Size.x; x += tileWidth)
+            {
+                for (float y = 0; y < Size.y; y += tileHeight)
+                {
+                    var obj = (GameObject)Instantiate(MapTilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    obj.name = "MapTile(" + x.ToString() + ", " + y.ToString() + ")";
+                    
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ client/World/WorldMap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Mayhem.World
+{
+    public class WorldMap : MonoBehaviour
+    {
+        public Vector2 MapSize;
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
